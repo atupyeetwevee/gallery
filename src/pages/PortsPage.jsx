7c67f562a6b62aa936e16c71dbb3fc1fc26cfb77
Atu Tweve <atutweve64@gmail.com>
2026-09-25 22:36:44 +0300

    designing portspage

--- src/pages/PortsPage.jsx
+++ src/pages/PortsPage.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
 
 function PortsPage() {
-  return (
-    <div>PortsPage</div>
+
+  const [showPorts, setShowPorts] = useState([]);
+
+    useEffect(() =>{
+      fetch("https://jsonplaceholder.typicode.com/comments?_limit=9")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setShowPorts(json);
+      })
+      .catch((error) => {
+          console.error("Failed to fetch photos:", error);
+        });
+    })
+    return (
+    <div>
+
+    </div>
   )
 }
 
