a04a86849bb70f080ed83d30cb208ac78a7e0bd2
Atu Tweve <atutweve64@gmail.com>
2026-09-26 16:39:25 +0300

    Designed the vesselPage

--- src/pages/VesselsPage.jsx
+++ src/pages/VesselsPage.jsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 
 function VesselsPage() {
+  const [showVessels, setShowVessels] = useState([]);
+
+    useEffect(() =>{
+      fetch("https://jsonplaceholder.typicode.com/comments?_limit=9")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setShowVessels(json);
+      })
+      .catch((error) => {
+          console.error("Failed to fetch photos:", error);
+        });
+    },[]);
   return (
-    <div>VesselsPage</div>
+    <div>
+      {showVessels.map((item) => ())}
+    </div>
   )
 }
 
